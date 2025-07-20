f3d9bef818211f30a8eb24a4011c3c9b590e8cc7
notj-code <junwonkim04@outlook.com>
2025-07-20 17:35:46 +0900

    first commit

---
+++ node_modules/grant/node_modules/qs/lib/parse.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var utils = require('./utils');
+
+var has = Object.prototype.hasOwnProperty;
+var isArray = Array.isArray;
+
+var defaults = {
+    allowDots: false,
+    allowEmptyArrays: false,
+    allowPrototypes: false,
+    allowSparse: false,
+    arrayLimit: 20,
+    charset: 'utf-8',
+    charsetSentinel: false,
+    comma: false,
+    decodeDotInKeys: false,
+    decoder: utils.decode,
+    delimiter: '&',
+    depth: 5,
+    duplicates: 'combine',
+    ignoreQueryPrefix: false,
+    interpretNumericEntities: false,
